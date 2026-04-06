6c2d1984942db20d00bd3c0b2e02f38db19fbeab
Shah <cshah2@volvocars.com>
2026-04-06 20:47:04 +0530

    updated-dags

--- dags/dag.py
+++ dags/dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
-from docker.types import Mount
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
@@ -10,25 +9,12 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    submit_spark_job = DockerOperator(
+    submit_spark_job = BashOperator(
         task_id="submit_trip_analysis",
-        image="apache/spark:3.5.0",
-        user="root",
-        command="""
-            bash -c "mkdir -p /home/spark/.ivy2 && /opt/spark/bin/spark-submit 
-            --master spark://spark-master:7077 
-            --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 
-            /opt/spark-jobs/process_trips.py"
+        bash_command="""
+            docker exec spark-master /opt/spark/bin/spark-submit \
+                --master spark://spark-master:7077 \
+                --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
+                /opt/spark-jobs/process_trips.py
         """,
-        network_mode="data-platforms_default",
-        mounts=[
-            Mount(
-                source="C:/Users/everb/OneDrive/Desktop/data-platforms/spark-jobs",
-                target="/opt/spark-jobs",
-                type="bind",
-                read_only=True,
-            ),
-        ],
-        docker_url="unix:///var/run/docker.sock",
-        auto_remove=True,
     )
